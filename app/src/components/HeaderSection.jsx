bb05e74fda1f75a78eb8edb2301a97883bf72c70
BrookMulu <brook.mgeb@gmail.com>
2024-01-30 21:40:28 -0600

    Deleted the services component

--- app/src/components/HeaderSection.jsx
+++ app/src/components/HeaderSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import {motion} from "framer-motion"
-import Services from "./services";
+import {CodeBracketSquareIcon, CommandLineIcon, RocketLaunchIcon} from "@heroicons/react/20/solid"
 import githubIcon from "../../../public/images/github-icon.svg";
 import linkedinIcon from "../../../public/images/linkedin-icon.svg";
 const HeaderSection = () => {
@@ -66,7 +66,35 @@ const HeaderSection = () => {
             </div>
             </div>
             <div className="-ml-6 lg:-ml-16">
-                <Services/>
+                <motion.div initial = {{opacity: 0, x:0}} animate = {{opacity: 1, x:15}} className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white">
+                    <div>
+                        <div className="bg-orange-600 hover:scale-110 transform tranition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
+                        <CodeBracketSquareIcon className="w-[6rem] h-[6rem] mc-auto text-[#d3fae8]"/>
+                        <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">Frontend</h1>
+                        <p className="text-[15px] text-[#d3d2d2] font-normal">
+                        Capable of leveraging HTML, CSS, and JavaScript ensuring optimal user engagement.
+                        </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bg-orange-600 hover:scale-110 transform tranition-all duration-300 uppercase font-semibold text-center p-[2rem]">
+                        <RocketLaunchIcon className="w-[6rem] h-[6rem] mc-auto text-[#d3fae8]"/>
+                        <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">Backend</h1>
+                        <p className="text-[15px] text-[#d3d2d2] font-normal">
+                        Able to craft efficient server-side logic, managing databases, and optimizing performance.
+                        </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bg-orange-600 hover:scale-110 transform tranition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]">
+                        <CommandLineIcon className="w-[6rem] h-[6rem] mc-auto text-[#d3fae8]"/>
+                        <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">Fullstack</h1>
+                        <p className="text-[15px] text-[#d3d2d2] font-normal">
+                        Proficient in integrating user interfaces with robust server-side functionalities.
+                        </p>
+                        </div>
+                    </div>
+                </motion.div>
             </div>
         </header>
     );
